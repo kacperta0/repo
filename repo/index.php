37f149b14b3f583440d5bfef01017b402cb9f09b
kacperta0 <kacpertaurzeski@interia.pl>
2017-07-19 00:17:55 +0200

    Add files via upload

--- repo/index.php
+++ repo/index.php
@@ -6,82 +6,51 @@
 <meta charset = "utf-8">
 <link rel = "stylesheet" href="orderstyle.css" type="text/css">
 
-<script>
-// Get the modal
-var modal = document.getElementById('id01');
+			<script> <!-- Mechanizm otwierania okna modal -->
+			// Wywołanie okna modal
+			var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
-
-
-
-
-
+			// Zamykanie po kliknięciu gdziekolwiek
+			window.onclick = function(event) {
+				if (event.target == modal) {
+					modal.style.display = "none";
+				}
+			}
+			</script>
 </head>
 
 <body>
 
-   
-   
-
-    <div id="logo">
-	
- 
-
-	
-	
-	</div>
-	<div id="container">
-
-		<form>
-			
-			
-			<center><input type="text" placeholder="Login" onfocus="this.placeholder=''" onblur="this.placeholder='login'" ></center>
-			
-			<center><input type="password" placeholder="Hasło" onfocus="this.placeholder=''" onblur="this.placeholder='login'" ></center>
-			<br/><br/><br/><br/>
-
-			
-			<center><input type="submit" value="Zaloguj się"></center>
-			<center><input type="submit" value="Rejestracja konta"></center>
-			
-			<center><input type="submit" value="Zamów bez logowania"></center>
-			
-		</form>
-		
-		
-
-<!-- The Modal -->
-<div id="id01" class="modal">
-  <span onclick="document.getElementById('id01').style.display='none'" 
-class="close" title="Close Modal">&times;</span>
-
-  <!-- Modal Content -->
-  <form class="modal-content animate" action="/action_page.php">
-    <div class="imgcontainer">
-      <img src="" alt="Avatar" class="avatar">
-	  
-    </div>
-  </form>
-</div>
-		
-		
+		<div id="logo"> <!-- Pusty div, określony w css -->
 		</div>
-
 	
-	
+			<div id="container"> <!-- Zawartość pola logowania -->
+				 <form> <!-- Przyciski oraz placeholdery -->
+							
+				<center><input type="text" placeholder="Login" onfocus="this.placeholder=''" onblur="this.placeholder='login'" ></center>
+				<center><input type="password" placeholder="Hasło" onfocus="this.placeholder=''" onblur="this.placeholder='login'" ></center>
+				<br/><br/><br/><br/>
 
+				<center><input type="submit" value="Zaloguj się"></center>
+				<center><input type="submit" value="Rejestracja konta"></center>
+							
+				<center><input type="submit" value="Zamów bez logowania"></center>
+							
+				</form>
+					
+					<!-- Okno modal -->
+					<div id="id01" class="modal">
+					<span onclick="document.getElementById('id01').style.display='none'" 
+					class="close" title="Close Modal">&times;</span>
 
-
-
+					  <!-- Zawartość okna modal po kliknięciu -->
+					 <form class="modal-content animate" action="/action_page.php">
+					 <div class="imgcontainer">
+					<img src="" alt="Avatar" class="avatar">
+						  
+					 </div>
+					</form>
+					</div>				
+			        </div>
 </body>
-
-
-
-
 </html>

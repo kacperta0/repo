0458ed2dd4ea762dfbc824c03a46ed23a0d486c4
kacperta0 <kacpertaurzeski@interia.pl>
2017-07-18 21:34:14 +0200

    Add files via upload

--- repo/index.php
+++ repo/index.php
@@ -1,1 +1,46 @@
 <!DOCTYPE HTML>
+
+<html lang = "pl">
+
+<head>
+<meta charset = "utf-8">
+<link rel = "stylesheet" href="orderstyle.css" type="text/css">
+
+
+
+
+</head>
+
+<body>
+
+<center><h2> Zamówienie </h2></center>
+
+	<div id="container">
+		<form>
+			
+			
+			<input type="text" placeholder="Login" onfocus="this.placeholder=''" onblur="this.placeholder='login'" >
+			
+			<input type="password" placeholder="Hasło" onfocus="this.placeholder=''" onblur="this.placeholder='login'" >
+			<br/><br/><br/><br/>
+
+			
+			<input type="submit" value="Zaloguj się">
+			<input type="submit" value="Rejestracja konta">
+			
+			<input type="submit" value="Zamów bez logowania">
+			
+		</form>
+	</div>
+	
+	
+
+
+
+
+</body>
+
+
+
+
+</html>
